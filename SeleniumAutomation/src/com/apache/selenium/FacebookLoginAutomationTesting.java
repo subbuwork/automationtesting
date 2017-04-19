3aa8fc3ab1523c89d1c156b18ded37325daa929d
subbu <subbu@192.168.0.233>
2017-04-19 18:01:28 -0400

    removed unwatned stuff..

--- SeleniumAutomation/src/com/apache/selenium/FacebookLoginAutomationTesting.java
+++ SeleniumAutomation/src/com/apache/selenium/FacebookLoginAutomationTesting.java
@@ -2,7 +2,6 @@ package com.apache.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -14,8 +13,8 @@ public class FacebookLoginAutomationTesting {
 		WebDriver webDriver = new FirefoxDriver();
 		webDriver.get("https://www.facebook.com");
 		webDriver.manage().window().maximize();
-		webDriver.findElement(By.xpath(".//*[@id='email']")).sendKeys("subbu.varsith1@gmail.com");
-		webDriver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("chitti@2016");
+		webDriver.findElement(By.xpath(".//*[@id='email']")).sendKeys("xxxxxxxxxx");
+		webDriver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("xxxxxxxx");
 		webDriver.findElement(By.xpath(".//*[@id='u_0_q']")).click();
 		
 		Select select = new Select(webDriver.findElement(By.xpath(".//*[@id='userNavigationLabel']")));
